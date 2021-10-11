7fc7e1e577825b037efb08f44d73918993d468b2
郭天宇 <1846548114@qq.com>
2021-10-11 09:47:52 +0800

    first commit

---
+++ src/test/java/com/groundhog/gateway/GroundhogGatewayCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groundhog.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroundhogGatewayCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
