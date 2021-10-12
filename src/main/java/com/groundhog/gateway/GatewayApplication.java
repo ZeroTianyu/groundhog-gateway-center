a9989b7a271c5b82e975f3056002a80673efaeb4
郭天宇 <1846548114@qq.com>
2021-10-12 10:51:21 +0800

    feat(网关配置): 网关配置完成

--- src/main/java/com/groundhog/gateway/GatewayApplication.java
+++ src/main/java/com/groundhog/gateway/GatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GroundhogGatewayCenterApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GroundhogGatewayCenterApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
