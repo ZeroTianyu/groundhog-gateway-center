275c5d682a92e56d62c4e5a48d6218ac475a7b47
guotianyu <1846548114@qq.com>
2021-10-14 20:31:15 +0800

    feat(网关配置): 增加author

--- src/main/java/com/groundhog/gateway/GatewayApplication.java
+++ src/main/java/com/groundhog/gateway/GatewayApplication.java
@@ -3,6 +3,9 @@ package com.groundhog.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author guotianyu
+ */
 @SpringBootApplication
 public class GatewayApplication {
 
